2cbebbf83a42e20b7f283167ce600f34c4bb4298
natamate <naciaa.nm@gmail.com>
2016-10-24 19:53:03 +0200

    Update Cw10Str142.java

--- Cw10Str142.java
+++ Cw10Str142.java
@@ -1,17 +1,40 @@
+
 //liczby wampirze
 public class Cw10Str142 {
-	static void wypiszLiczbyWampirze(){
-		for(int i = 10; i < 99; i++){
-		    for(int j = 10; j < 99; j++){
-		        int result = i*j;
-		        String resultStr = Integer.toString(result);
-		        if(resultStr.length() == 4 && !resultStr.substring(2, 4).equals("00")){
-		            System.out.println(result + " = " + i + " * " + j);
-		        }
-		    }
+	static void check(int i, int j){
+		int result = i * j;
+		int counter = 0;
+		if ( result < 1000 || result % 100 == 0) return;
+		
+		String liczba = Integer.toString(result);
+		String slowo = Integer.toString(i) + Integer.toString(j);
+		
+		char licz[] = liczba.toCharArray();
+		char slow[] = slowo.toCharArray();
+		
+		et1:
+			 for(int st1 = 0; st1 < 4 ; st1++){    
+				 for(int st2 = 0; st2 < 4; st2++){
+		            if(slow[st1] == licz[st2]){
+		            	licz[st2] = Character.MAX_VALUE;
+		                counter ++;
+		                continue et1;
+		            }
+				 }
+			 }
+		if(counter == 4){
+		    System.out.println(result + " = " + i+ " * " + j);    
 		}
 	}
+	
 	public static void main(String[] args){
-		wypiszLiczbyWampirze();
+		for (int i = 10; i < 100; i++){
+			for (int j = 10; j < 100; j++){
+				if (j > i){
+					break;
+				}
+				check(i,j);
+			}
+		}
 	}
 }
