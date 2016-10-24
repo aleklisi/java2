55caa1c6eeaaa3c53a28153a3d71bf8923e21cb7
aleklisi <sebaczechu123@gmail.com>
2016-10-24 20:22:35 +0200

    Update Cw8Str142.java

--- Cw8Str142.java
+++ Cw8Str142.java
@@ -1,34 +1,11 @@
-import java.util.*;
-
-public class Cw8Str142 {
-	static void test(){
-		Random rand = new Random(47);
-		for (int i = 0; i < 10; i++){
-			int c = rand.nextInt(26) + 'a';
-			char sign = (char)c;
-			switch(sign){
-			case 'a':
-			case 'e':
-			case 'i':
-			case 'o':
-			case 'u':
-			case 'y': {
-				System.out.println(i + " : " + sign + " samogloska");
-				//break;
-			}
-			case 'x': {
-				System.out.println(i + " : " + sign + " nie wiem");
-				//break;
-			}
-			default:  {
-				System.out.println(i + " : " + sign + " spolgloska");
-				//break;
-			}
-
+public class swicz {
+	public static void main(String []args){
+		for (int i = 1; i < 4; i++){
+			switch (i){
+			case 1 : System.out.println("1") ; 
+			case 2 : System.out.println("2") ; 
+			case 3 : System.out.println("3") ; 
 			}
 		}
-	}
-	public static void main(String[] args){
-		test();
 	}
 }
