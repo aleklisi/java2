45ddc755ba8dfb8414e250ccedd87f3fa23dbd14
natamate <naciaa.nm@gmail.com>
2016-11-29 09:41:19 +0100

    Update Logs.java

--- Logs.java
+++ Logs.java
@@ -4,7 +4,7 @@ public interface Logs {
 	public enum Statement{
 		ERROR, INFO, WARNING;
 	}
-	public boolean checkOutList(Statement statement, String info);
+	public void setIgnoreStatement(Statement statement);
 	public void displayStatement();
 	public void writeStatementToFile();
 	public void readStatementFromFile();
